d13f5ca1cce37380a272675a0c362a39d1c8e227
Håkon Ringdal <hakon.ringdal@gmail.com>
2019-02-04 16:00:39 +0100

    hoh

---
+++ src/Genome.java
@@ -0,0 +1,10 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class Genome {
+    private ArrayList<ArrayList<Integer>> genome = new ArrayList<>();
+
+    public Genome() {
+
+    }
+}
