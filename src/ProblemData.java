079daff1428c4a9b7a1e723d50d35256e4f8fad1
BrynjarGlims <brynjar.glimsdal@gmail.com>
2019-02-09 14:55:14 +0100

    merge

--- src/ProblemData.java
+++ src/ProblemData.java
@@ -88,5 +88,52 @@ public class ProblemData {
         return depotData;
     }
 
+    public Genome readSolutionFile(String path){
+        try {
+            FileInputStream fstream = new FileInputStream(path);
+            BufferedReader br = new BufferedReader(new InputStreamReader(fstream));
+
+            System.out.println("fitness: " + br.readLine());
+            String str = "";
+            while(str != null){
+                str = br.readLine();
+
+            }
+
+            String[] data = str.split(" ");
+            this.numVehicles = Integer.parseInt(data[0]);
+            this.numCustomers = Integer.parseInt(data[1]);
+            this.numDepots = Integer.parseInt(data[2]);
+
+
+            for (int i = 0 ; i < numDepots ; i++){
+                str = br.readLine();
+                data = str.split(" ");
+                this.maxDurations.add(Integer.parseInt(data[0]));
+                this.maxLoads.add(Integer.parseInt(data[1]));
+            }
+
+            for (int i = 0 ; i < numCustomers ; i++){
+                ArrayList<Integer> custInt = new ArrayList<>();
+                str = br.readLine();
+                ArrayList<String> custString = new ArrayList<>(Arrays.asList(str.trim().split("\\s+")));
+                for(String s : custString)custInt.add(Integer.parseInt(s));
+
+                this.customerData.add(custInt.subList(0, 5));
+            }
+
+            for (int i = 0 ; i < numDepots ; i++){
+                ArrayList<Integer> depotInt = new ArrayList<>();
+                str = br.readLine();
+                ArrayList<String> depotString = new ArrayList<>(Arrays.asList(str.trim().split("\\s+")));
+
+                for(String s : depotString)depotInt.add(Integer.parseInt(s));
+
+                this.depotData.add(depotInt.subList(0,3));
+            }
+        }
+        catch (Exception e){throw new IllegalStateException("File format not correct");}
+        return null;
+    }
 
 }
